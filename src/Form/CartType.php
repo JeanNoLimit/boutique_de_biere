06071a4a1afa833ac3868025d5d209cacb9a8fb2
Jean-Noël richert <jean.noel.richert@gmail.com>
2023-08-25 16:55:00 +0200

    Création du formulaire d'ajout produit au panier

---
+++ src/Form/CartType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\LessThan;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+
+
+class CartType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quantity', IntegerType::class, [
+                'attr' => ['class' => "input_number", 'min' => "1", 'max' =>"99", 'step' => "1", 'value' =>"1"],
+                'label' => 'Quantité',
+                'invalid_message' => "Veuillez rentrer une quantité valide (commprise entre 1 et 99)",
+                'required' => true,
+                'empty_data' => 1,
+                'constraints' => [
+                    new Positive(),
+                    new GreaterThanOrEqual(1),
+                    new LessThan(99)
+                ]
+            ])
+            // ->add('id', IntegerType::class)
+            ->add('Envoyer', SubmitType::class, [
+                'attr' => ['class' => "button_validation"],
+                'label' => 'Ajouter au panier'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
