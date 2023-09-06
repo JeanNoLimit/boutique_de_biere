a8e4e06317721b9a1e39537818d82831a9846c49
Jean-Noël richert <jean.noel.richert@gmail.com>
2023-09-06 20:39:57 +0200

    Add Webhook Controller - en attente de debug

---
+++ src/Controller/WebhookController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Stripe\Stripe;
+use Stripe\Webhook;
+use Stripe\StripeClient;
+use Stripe\Exception\UnexpectedValueException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Stripe\Exception\SignatureVerificationException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class WebhookController extends AbstractController
+{
+    /******************* TEST D2CLENCHEMENT ACITONS AVEC WEBHOOKS *******************/
+
+    #[Route('/stripe/webhooks', name: 'app_stripe_webhooks')]
+    public function webhooks()
+    {
+
+        Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+       
+        // This is your Stripe CLI webhook secret for testing your endpoint locally.
+        $endpoint_secret = '';
+    
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $payload = @file_get_contents('php://input');
+        $event = null;
+
+        try {
+          $event = \Stripe\Webhook::constructEvent(
+            $payload, $sig_header, $endpoint_secret
+          );
+        }
+          
+          catch(\UnexpectedValueException $e) {
+            // Invalid payload
+            http_response_code(400);
+            exit();
+        }
+        
+        // Handle the event
+        switch ($event->type) {
+            case 'payment_intent.succeeded':
+                $paymentIntent = $event->data->object; // contains a \Stripe\PaymentIntent
+                handlePaymentIntentSucceeded($paymentIntent);
+                break;
+            case 'payment_method.attached':
+                $paymentMethod = $event->data->object; // contains a \Stripe\PaymentMethod
+                handlePaymentMethodAttached($paymentMethod);
+                break;
+            // ... handle other event types
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+        
+        http_response_code(200);
+
+    }
+}
