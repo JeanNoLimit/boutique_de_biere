4f979164b684708198bea230604df535aa7bfbb9
Jean-Noël richert <jean.noel.richert@gmail.com>
2023-09-13 16:25:46 +0200

    Responsive page list products

---
+++ public/js/filterDropdown.js
@@ -0,0 +1,19 @@
+// Fonction affichage sous menu filtres
+function filterDropdownFunction() {
+    if (window.innerWidth <= 1104) {
+        let btn = document.getElementById("btn_filters");
+        let chevron = document.getElementById("chevron1");
+        let filterMenu = document.getElementById("filters_container");
+
+
+        if (btn.className === "dropdown_btn_products filterBtn") {
+            btn.className = "dropdown_btn_products filterBtnResp";
+            chevron.className = "fa-solid fa-chevron-up";
+            filterMenu.className += "filterMenuResponsive";
+        } else {
+            btn.className = "dropdown_btn_products filterBtn";
+            chevron.className = "fa-solid fa-chevron-down";
+            filterMenu.className = "";
+            }
+    }
+}
