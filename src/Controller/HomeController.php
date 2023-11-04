311735e8a77e1846a5be060288517b45e53420c5
Jean-Noël richert <jean.noel.richert@gmail.com>
2023-11-04 18:03:44 +0100

    Ajout pages footers (vide)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -26,9 +26,53 @@ class HomeController extends AbstractController
     }
 
 
-    #[Route('/mentions_legales', name: 'app_mentions')]
-    public function legalNotices(): Response
+    #[Route('/cgv', name: 'app_cgv')]
+    public function cgv(): Response
     {
-        return $this->render('home/legal_notices.html.twig', []);
+        return $this->render('home/cgv.html.twig', []);
     }
+
+    #[Route('/cgu', name: 'app_cgu')]
+    public function cgu(): Response
+    {
+        return $this->render('home/cgu.html.twig', []);
+    }
+
+    #[Route('/rgpd_lechoppe', name: 'app_rgpd')]
+    public function rgpd(): Response
+    {
+        return $this->render('home/rgpd.html.twig', []);
+    }
+
+    #[Route('/sitemap', name: 'app_sitemap')]
+    public function sitemap(): Response
+    {
+        return $this->render('home/sitemap.html.twig', []);
+    }
+
+    #[Route('/mentions_legales', name: 'app_mentions')]
+    public function legalNotices(EntityManagerInterface $entityManager): Response
+    {
+        $parameters = $entityManager->getRepository(ShopParameters::class)->findAll()[0];
+
+        return $this->render('home/legal_notices.html.twig', ['parameters' => $parameters]);
+    }
+
+    #[Route('/presentation', name: 'app_presentation')]
+    public function presentation(EntityManagerInterface $entityManager): Response
+    {
+        $parameters = $entityManager->getRepository(ShopParameters::class)->findAll()[0];
+
+        return $this->render('home/presentation.html.twig', ['parameters' => $parameters]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+      
+        return $this->render('home/contact.html.twig', []);
+    }
+
+
+
 }
